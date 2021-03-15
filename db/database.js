be67d6294219d16b4385c25ea0a62419f8f10ceb
VlaDrozd <vladrozd2000@gmail.com>
2021-03-15 11:34:17 +0300

    update enviroment

--- db/database.js
+++ db/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const DB = process.env.DB;
+const DB = process.env.DB_HOST;
 
 const database = new Sequelize(`postgres://postgres:postgres@${DB}:5432`);
 
